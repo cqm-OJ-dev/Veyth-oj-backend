96885d4b4ead44bb678e2f4f9b7e081528c72499
cqiming <cqming2025@outlook.com>
2025-11-30 13:00:35 +0800

    Update views.py

--- judge/views.py
+++ judge/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.http import require_POST
 from rest_framework.permissions import AllowAny
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework import status
 from .sandbox import run_in_sandbox
 
 @csrf_exempt
