df97b8334277db73415ec1d31053243135602f65
zsz-qwq <zhangsizhe@end.tw>
2026-08-29 13:28:16 +0800

    Update admin.py

--- judge/admin.py
+++ judge/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Submission, JudgeResult
 
-# Register your models here.
+class JudgeResultInline(admin.TabularInline):
+    model = JudgeResult
+    extra = 0
+    readonly_fields = ('case_index', 'status', 'time_ms', 'wall_time_ms', 'memory_kb', 'returncode', 'stdout', 'stderr')
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+class SubmissionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'problem', 'language', 'status', 'passed_cases', 'total_cases', 'max_time_ms', 'created_at')
+    list_filter = ('language', 'status', 'created_at')
+    search_fields = ('user__username', 'problem__title')
+    inlines = [JudgeResultInline]
+    readonly_fields = ('status', 'passed_cases', 'total_cases', 'max_time_ms', 'max_memory_mb', 'error_message', 'created_at')
+
+admin.site.register(Submission, SubmissionAdmin)
