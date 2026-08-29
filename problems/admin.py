ce7da8ab1b08d20fa31a9eec94e32b87d4c3ed99
zsz-qwq <zhangsizhe@end.tw>
2026-08-29 13:20:43 +0800

    Update admin.py

--- problems/admin.py
+++ problems/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
-from .models import Problem
+from .models import Problem, Example, TestCase
 
-# Register your models here.
+class ExampleInline(admin.StackedInline):
+    model = Example
+    extra = 1
+    ordering = ('order',)
+
+class TestCaseInline(admin.StackedInline):
+    model = TestCase
+    extra = 1
+    ordering = ('order',)
 
 class ProblemAdmin(admin.ModelAdmin):
-    list_display = ('title', 'difficulty', 'created_at', 'updated_at')
+    list_display = ('id', 'title', 'difficulty', 'time_limit_ms', 'memory_limit_mb',
+                    'accepted', 'submissions', 'created_at', 'updated_at')
     search_fields = ('title', 'description', 'difficulty')
     list_filter = ('difficulty', 'created_at', 'updated_at')
+    inlines = [ExampleInline, TestCaseInline]
 
 admin.site.register(Problem, ProblemAdmin)
